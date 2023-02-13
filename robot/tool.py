9e91b0385443c889f76a276f41e89e8e0d9bd89f
yqlydcjm <109834497+yqlydcjm@users.noreply.github.com>
2023-02-13 22:00:01 +0800

    Delete robot directory

--- robot/tool.py
+++
@@ -1,8 +0,0 @@
-import requests
-import script
-def qf(uid,message):
-    friend_list = requests.get(url='http://127.0.0.1:5700/get_friend_list')
-    friends = friend_list.json()
-    for i in friends['data']:       #获取用户用户名以及qq号
-        uid = i['user_id']
-        script.bl(uid, message)
